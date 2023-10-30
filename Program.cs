0136aff6a66b279dd58d0d55e3ea64228df46216
Aurellion <johannes.bock@nastor.eu>
2023-10-30 09:19:12 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Schaltjahr_WS2324_T11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aufgabe
+            //Erstellen Sie ein Programm mit Menü, das nach Eingabe
+            //einer Jahreszahl, ausgibt ob es sich um ein
+            //Schaltjahr handelt.
+        }
+    }
+}
